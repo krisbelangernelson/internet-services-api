7c2fadd2fd8bb76b45f04413134716851196a2c1
KrNel <159314411+krisbelangernelson@users.noreply.github.com>
2024-02-27 16:01:53 -0500

    refactor: controllers void

--- src/controllers/internetController.ts
+++ src/controllers/internetController.ts
@@ -3,8 +3,8 @@ import internetServices from '@/services/internetServices'
 import { errorResponses } from '@/utils/httpErrors/errorResponses'
 import { type Error } from '@/types/error'
 
-const getInternetServices = async (_req: Request, res: Response): Promise<Response> => {
-  return await internetServices
+const getInternetServices = async (_req: Request, res: Response): Promise<void> => {
+  await internetServices
     .getInternetServices()
     .then((results) => res.json(results))
     .catch((error: Error) => errorResponses(res, error, 'getInternetServices'))
