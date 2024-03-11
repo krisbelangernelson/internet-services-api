80ce311d9998b84cdd4b0b560af1ff48ee249499
KrNel <159314411+krisbelangernelson@users.noreply.github.com>
2024-03-11 13:24:38 -0400

    fix: env var db port

--- src/db/connection/config.ts
+++ src/db/connection/config.ts
@@ -1,5 +1,5 @@
 export const host = process.env.DB_HOST
 export const database = process.env.DB_DATABASE
-export const port = 5432
+export const port = process.env.DB_PORT !== undefined ? Number(process.env.DB_PORT) : 5432
 export const user = process.env.DB_USER
 export const password = process.env.DB_PASS
